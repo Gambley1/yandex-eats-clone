dfd76ae7e9d80d461b8a370e64f09b3219b6ebae
Emil Zulufov (ezIT) <emilzulufov@gmail.com>
2024-05-21 21:02:04 +0500

    refactor: main (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:papa_burger/src/app/app.dart';
 import 'package:papa_burger/src/services/network/api/api.dart';
 import 'package:papa_burger/src/services/repositories/user/user.dart';
 
-void main() async {
-  final userApi = UserApi();
-  final userRepository = UserRepository(userApi: userApi);
-
-  await bootstrap(() => App(userRepository: userRepository));
-}
+void main() {
+  bootstrap(() {
+    final userApi = UserApi();
+    final userRepository = UserRepository(userApi: userApi);
+    return MyApp(userRepository: userRepository);
+  });
