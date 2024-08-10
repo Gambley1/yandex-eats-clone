99cb75dcffe70c347d26c4b0dfc74a433d6fe361
Emil <emilzulufov.commercial@gmail.com>
2024-08-10 14:01:56 +0500

    chore: remove unused models

--- packages/shared/lib/src/models/models.dart
+++ packages/shared/lib/src/models/models.dart
@@ -1,7 +1,4 @@
 export 'address.dart';
 export 'auto_complete.dart';
-export 'cart.dart';
-export 'category.dart';
-export 'exceptions.dart';
 export 'menu_tab_category.dart';
 export 'place_details.dart';
